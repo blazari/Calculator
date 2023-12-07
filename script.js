2d8e3b7257b8ed97cd622b60bf1e8e23570fa8a5
blazari <117378910+blazari@users.noreply.github.com>
2023-12-07 20:18:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,37 @@
-// Select all the from document using queryselectAll
 var keys = document.querySelectorAll('#calculator span');
-// Define operators
 var operators = ['+', '-', 'x', '÷', '^'];
-// Set decimal flag for use later
 var decimalAdded = false;
-
-// loop through all keys
 for(var i = 0; i < keys.length; i++) {
-  //add onclick event to the keys
 	keys[i].onclick = function(e) {
-		// Get the input and button values
 		var input = document.querySelector('.screen');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
-    
-		// If clear key is pressed, erase everything
 		if(btnVal == 'C') {
 			input.innerHTML = '';
 			decimalAdded = false;
 		}
-		
-		// If eval key is pressed, calculate and display the result
 		else if(btnVal == '=') {
-			var equation = inputVal; //append equation to variable
-			var lastChar = equation[equation.length - 1]; //target the end of the eval string
-     	
-			// Use regex to replace all instances of x, ÷, ^ with *, / and **
+			var equation = inputVal; 
+			var lastChar = equation[equation.length - 1];
 			equation = equation.replace(/x/g, '*').replace(/÷/g, '/').replace(/\^/g, '\*\*');
-			
-			//Use regex to remove decimals from the end of an equation
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
 				equation = equation.replace(/.$/, '');
-      
-			// use javascript's eval function to get the result
-      
 			if(equation)
 				input.innerHTML = eval(equation);	
 			  decimalAdded = false;
-		}
-		
-		// Javascript checks
-
-    // No two operators should be added consecutively.		
+		}	
 		else if(operators.indexOf(btnVal) > -1) {
-			// Get the last character from the equation
 			var lastChar = inputVal[inputVal.length - 1];
-			
-			// Only add operator if input is not empty 
 			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
 				input.innerHTML += btnVal;
-			
-			// Allow minus operator if the string is empty
 			else if(inputVal == '' && btnVal == '-') 
 				input.innerHTML += btnVal;
-			
-			// Replace the last operator (if exists) with the newly pressed operator
 			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
 				input.innerHTML = inputVal.replace(/.$/, btnVal);
 			}
 			
 			decimalAdded =false;
 		}
-    // allow decimal point input
 		else if(btnVal == '.') {
 			if(!decimalAdded) {
 				input.innerHTML += btnVal;
@@ -69,17 +39,14 @@ for(var i = 0; i < keys.length; i++) {
 			}
 		}
 		
-		// if any other key is pressed, just append it after the decimal
 		else {
 			input.innerHTML += btnVal;
 		}
 		
-		// prevent page jumps
 		e.preventDefault();
 	} 
 }
 
-//adding keyboard input functionality
 document.onkeydown = function(event) {
 
 	var key_press = String.fromCharCode(event.keyCode);
@@ -89,11 +56,7 @@ document.onkeydown = function(event) {
 	var btnVal = this.innerHTML;
   var lastChar = inputVal[inputVal.length - 1];
   var equation = inputVal;
-	// Using regex to replace all instances of x, ÷, ^ with *, / and ** respectively. 
 	equation = equation.replace(/x/g, '*').replace(/÷/g, '/').replace(/\^/g, '**');
-
- // Target each keypress and update the input screen
-  
     if(key_press==1) {
       input.innerHTML += key_press;
   }
@@ -124,9 +87,6 @@ document.onkeydown = function(event) {
     if(key_press==0) {
       input.innerHTML += key_press;
   }
-  
-  // Cature operators and prevent from addint two consecutuve operators
-  
     if ((inputVal != '' && operators.indexOf(lastChar) == -1 && key_code == 187 && event.shiftKey) || (key_code == 107) || (key_code == 61 && event.shiftKey)) {
       document.querySelector('.screen').innerHTML += '+';
   }
@@ -144,7 +104,6 @@ document.onkeydown = function(event) {
   }
     if(key_code==13 || key_code==187 && event.shiftKey == false) {
       input.innerHTML = eval(equation);
-      //reset decimal added flag
       decimalAdded =false;
   }
     if(key_code==8 || key_code==46) {
